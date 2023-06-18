70c8aabaa656e55650abc89aa86690a86dec9e66
Jarl André Hübenthal <jarlah@protonmail.com>
2023-06-18 23:00:05 +0200

    another test

--- src/test/scala/game/BreakoutSceneTest.scala
+++ src/test/scala/game/BreakoutSceneTest.scala
@@ -71,4 +71,33 @@ class BreakoutSceneTest extends AnyFunSuite {
     // Verify that the ball's speedX has been inverted to move leftwards
     assert(scene.ball.speedX == -3)
   }
+
+  test("Ball is placed on paddle and paddle is centered when ball hits the bottom of the screen") {
+
+    val assetManagerMock = mock(classOf[AssetManager])
+    val keyManagerMock = mock(classOf[GameKeyManager])
+    val sceneUtilsMock = mock(classOf[SceneUtils])
+
+    when(sceneUtilsMock.width).thenReturn(800)
+    when(sceneUtilsMock.height).thenReturn(600)
+
+    val scene = BreakoutScene(assetManagerMock, keyManagerMock, sceneUtilsMock)
+
+    // Assume there are no bricks left
+    scene.bricks = Array.empty
+    // Assume ball is at bottom of the screen and moving downwards
+    scene.ball = scene.ball.copy(y = sceneUtilsMock.height - scene.ball.radius, speedY = 3, moving = true)
+
+    scene.update(1.0)
+
+    // Verify that the ball's is placed on the paddle
+    assert(scene.ball.y == scene.paddle.y - scene.ball.radius)
+    assert(scene.ball.x == scene.paddle.x + scene.paddle.width / 2)
+
+    // Verify that the paddle is centered
+    assert(scene.paddle.x == sceneUtilsMock.width / 2)
+
+    // Verify that the ball is not moving
+    assert(!scene.ball.moving)
+  }
 }
