38dbf89b6122af1588f996cb66da4a510325c6ca
Jarl André Hübenthal <jarlah@protonmail.com>
2023-06-18 17:38:41 +0200

    refactor further

--- src/main/scala/game/main.scala
+++ src/main/scala/game/main.scala
@@ -1,6 +1,6 @@
 package game
 
-import java.awt.event.{ComponentAdapter, ComponentEvent}
+import java.awt.event.{ComponentAdapter, ComponentEvent, ComponentListener, KeyListener}
 import java.awt.{Dimension, Image}
 import javax.swing.{ImageIcon, JFrame, WindowConstants}
 
@@ -9,8 +9,32 @@ def main(): Unit = {
   val assetManager = new AssetManager
   val keyManager = new GameKeyManager
   val sceneManager = new SceneManager(keyManager)
+
   sceneManager.setScene(new WelcomeScene(assetManager, keyManager, sceneManager))
 
+  val frame = createGameWindow(sceneManager)
+  val ticker = new Ticker(
+    sceneManager.update,
+    render(frame, sceneManager)
+  )
+
+  ticker.start()
+
+  sys.addShutdownHook(ticker.stop())
+}
+
+def render(frame: JFrame, sceneManager: SceneManager): Unit = {
+  val bs = frame.getBufferStrategy
+  val g = bs.getDrawGraphics
+  sceneManager.render.run(new GraphicsIOWrapper(g)) match {
+    case Left(e) => e.printStackTrace()
+    case _ =>
+  }
+  g.dispose()
+  bs.show()
+}
+
+def createGameWindow[T <: KeyListener with ComponentListener](sceneManager: T): JFrame = {
   val frame = new JFrame("Test")
   frame.setSize(800, 600)
   frame.setPreferredSize(new Dimension(800, 600))
@@ -23,23 +47,5 @@ def main(): Unit = {
   frame.addComponentListener(sceneManager)
   frame.setFocusable(true)
   frame.requestFocus()
-
-  def update(delta: Double): Unit = {
-    sceneManager.update(delta)
-  }
-
-  def render: Unit = {
-    val bs = frame.getBufferStrategy
-    val g = bs.getDrawGraphics
-    sceneManager.render.run(new GraphicsIOWrapper(g)) match {
-      case Left(e) => e.printStackTrace()
-      case _ =>
-    }
-    g.dispose()
-    bs.show()
-  }
-
-  val ticker = new Ticker(update, render)
-  ticker.start()
-  sys.addShutdownHook(ticker.stop())
+  frame
 }
