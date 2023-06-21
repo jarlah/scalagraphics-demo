0bfd46043aba0b57c789a141269e7c6c25332e14
Jarl André Hübenthal <jarlah@protonmail.com>
2023-06-21 07:40:38 +0200

    refactor game key manager to proper scala

--- src/main/scala/GameKeyManager.scala
+++ src/main/scala/GameKeyManager.scala
@@ -14,30 +14,28 @@ class GameKeyManager extends KeyListener {
   private val cantPress =
     mutable.Map[GameKey, Boolean]().withDefaultValue(false)
 
-  private def convertToGameKey(keyCode: Int) = keyCode match {
-    case KeyEvent.VK_UP     => GameKey.Up
-    case KeyEvent.VK_DOWN   => GameKey.Down
-    case KeyEvent.VK_RIGHT  => GameKey.Right
-    case KeyEvent.VK_LEFT   => GameKey.Left
-    case KeyEvent.VK_ENTER  => GameKey.Enter
-    case KeyEvent.VK_ESCAPE => GameKey.Escape
-    case KeyEvent.VK_SPACE  => GameKey.Space
-    case _                  => null
+  private def convertToGameKey(keyCode: Int): Option[GameKey] = keyCode match {
+    case KeyEvent.VK_UP     => Some(GameKey.Up)
+    case KeyEvent.VK_DOWN   => Some(GameKey.Down)
+    case KeyEvent.VK_RIGHT  => Some(GameKey.Right)
+    case KeyEvent.VK_LEFT   => Some(GameKey.Left)
+    case KeyEvent.VK_ENTER  => Some(GameKey.Enter)
+    case KeyEvent.VK_ESCAPE => Some(GameKey.Escape)
+    case KeyEvent.VK_SPACE  => Some(GameKey.Space)
+    case _                  => None
   }
   override def keyTyped(e: KeyEvent): Unit = {}
 
-  override def keyPressed(e: KeyEvent): Unit = {
-    val gameKey = convertToGameKey(e.getKeyCode)
-    if (gameKey != null) keys.put(gameKey, true)
-  }
+  override def keyPressed(e: KeyEvent): Unit =
+    convertToGameKey(e.getKeyCode)
+      .foreach(gameKey => keys.put(gameKey, true))
 
-  override def keyReleased(e: KeyEvent): Unit = {
-    val gameKey = convertToGameKey(e.getKeyCode)
-    if (gameKey != null) {
-      keys.put(gameKey, false)
-      cantPress.put(gameKey, false)
-    }
-  }
+  override def keyReleased(e: KeyEvent): Unit =
+    convertToGameKey(e.getKeyCode)
+      .foreach { gameKey =>
+        keys.put(gameKey, false)
+        cantPress.put(gameKey, false)
+      }
 
   def update(): Unit = {
     for (key <- GameKey.values) {
